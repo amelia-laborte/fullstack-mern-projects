3cdfbf50dd7b42f3e389813817cde0d3beb7ce73
amelia-laborte <93796860+amelia-laborte@users.noreply.github.com>
2022-08-21 14:33:53 -1000

    Added error messages from validation

--- productManager/client/src/components/Form.js
+++ productManager/client/src/components/Form.js
@@ -7,6 +7,7 @@ const Form = () => {
     const [title, setTitle] = useState ("")
     const [price, setPrice] = useState ("")
     const [description, setDescription] = useState ("")
+    const [errors, setErrors] = useState({})
 
     const navigate = useNavigate()
 
@@ -22,6 +23,7 @@ const Form = () => {
             navigate("/productlist")//this will navigate to a url route onSubmit
         }).catch((err)=>{
             console.log(err)
+            setErrors(err.response.data.error)
         })
 
     }
@@ -32,14 +34,19 @@ return(
             <h1>Product</h1>
             <label>Title:</label>
             <input type= "text" onChange ={(e) => setTitle(e.target.value)}></input>
+            <br></br>{errors.title ?<span> {errors.title.message}</span> : null}
         </div>
         <div>
             <label>Price:</label>
             <input type= "text" onChange ={(e) => setPrice(e.target.value)}></input>
+            <br></br>{errors.price ?<span> {errors.price.message}</span> : null}
+
         </div>
         <div>
             <label>Description:</label>
             <input type= "text" onChange ={(e) => setDescription(e.target.value)}></input>
+            <br></br>{errors.description ?<span> {errors.description.message}</span> : null}
+
         </div>
         <input type ="Submit" value="Create Product"></input>
     </form>
